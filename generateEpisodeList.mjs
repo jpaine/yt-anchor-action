dc6c85e47b853e946beada05c56a9bd2a4166720
Mandar Devarshi <34445750+MandarDevarshi@users.noreply.github.com>
2022-11-26 15:25:20 +0000

    fix: episode.json generation

--- generateEpisodeList.mjs
+++ generateEpisodeList.mjs
@@ -11,24 +11,24 @@ import fs from 'node:fs'
 
 // Download playlist information
 // Entrypoint
-downloadPlaylistJSON();
+downloadPlaylistJSON()
 
 
 function downloadPlaylistJSON() {
-  // TODO: Remove
-  // episodeIterator();
+  // TODO: Remove in the end before handover
+  episodeIterator();
 
-  getPlaylist(process.env.YT_PLAYLIST)
-    .then(data => {
-      fs.writeFile('./playlist.json', JSON.stringify(data), err => {
-        if (err) {
-          console.error("Error in writing playlist data", err);
-        }
-        console.log("Playlist downloaded successfully")
-        episodeIterator();
-      });
-    })
-    .catch(console.error);
+  // getPlaylist(process.env.YT_PLAYLIST)
+  //   .then(data => {
+  //     fs.writeFile('./playlist.json', JSON.stringify(data), err => {
+  //       if (err) {
+  //         console.error("Error in writing playlist data", err)
+  //       }
+  //       console.log("Playlist downloaded successfully")
+  //       episodeIterator()
+  //     });
+  //   })
+  //   .catch(console.error)
 }
 
 // 
@@ -48,7 +48,7 @@ function episodeIterator() {
     // Iterate over the length of playlist i.e video count in a playlist
     for (let index = 0; index < videoCount; index++) {
       // Extract video ID to be used by episode.json 
-      getVideoID(playlistDataObj.videos[index])
+      getVideoID(playlistDataObj.videos[index], index)
     }
   });
 }
@@ -56,13 +56,37 @@ function episodeIterator() {
 /**
  * 
  * @param {object} data - single video object 
+ * @param {number} index - index of video object
  */
 
-function getVideoID(data) {
+function getVideoID(data, index) {
   var videoObj = {
-    id:""
+    id:data.ID
   }
   // console.log("Video ID", data.URL, typeof (data.URL), data.URL.slice(32))
-  videoObj.id = data.URL.slice(32)
-  console.log(videoObj)
+  // console.log(videoObj)
+  createActionInputFile(videoObj, index)
+}
+
+/**
+ * 
+ * @param {object} episodeID - video ID from getVideoID
+ */
+
+function createActionInputFile(episodeID, index){
+  // TODO:
+  //  Write to episodes.json
+  //  Cleanup
+  
+  // console.log(episodeID)
+  // const fileName = `episode-${index}.json`
+  
+  const fileName = `episode.json`
+
+  fs.writeFile(fileName, JSON.stringify(episodeID), err => {
+          if (err) {
+            console.error("Error in writing to episode.json", err)
+          }
+          console.log("Write successfull", fileName)
+  });
 }
