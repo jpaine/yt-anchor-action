c51281aa91115a0776ddcaacc0cf6de93be8d636
Mandar Devarshi <34445750+MandarDevarshi@users.noreply.github.com>
2022-11-26 09:07:16 +0000

    Changes to workflow and automation script testing

---
+++ generateEpisodeList.js
@@ -0,0 +1,28 @@
+/**
+ * This function consumes a youtube playlist URL
+ * and extracts video ids from it, to be used
+ * as input for episode.json 
+ * 
+ * @param {string} playlistURL - URL of playlist to be processed for upload  
+ */
+import * as doc from './playlist.html'
+
+function generateEpisodeList(playlistURL) {
+
+    // Download the playlist document and store it at local root
+    // In this scenario no playlistURL is needed, remove it
+    const document = doc
+    // Store the playlist-items node, returns an HTMLCollection
+    // HTMLCollection is an array-like object
+    const playlistNodes = document.getElementsByClassName("playlist-items")
+    // Create an array from HTMLCollection
+    const playlistArray = Array.from(playlistNodes)
+    
+    // Total videos to process from array
+    console.log("Total Videos", playlistArray.length)
+    // Total videos to process from HTMLCollection
+    console.log("Total nodes in HTMLCollection",playlistNodes.length)
+    // Iterate over array items
+    // Return an array of id
+    return 0
+}
