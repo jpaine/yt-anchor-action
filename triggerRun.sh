ee8e11b658cef77dc9e1d7f7e1cf9e44209b2117
Mandar Devarshi <34445750+MandarDevarshi@users.noreply.github.com>
2022-11-29 07:41:47 +0000

    chore: commit message changes

--- triggerRun.sh
+++ triggerRun.sh
@@ -6,11 +6,11 @@ node generateEpisodeList.mjs
 TOTAL_EPISODES=$(ls | grep episode | wc -l)
 
 echo
-echo "=======           Commit Changes to Processed Videos ===============================\n"
+echo "=======           Commit Changes to Processed Videos                        ======\n"
 echo " Note: To reset in case of failure use emptyProcessedFile script and commit manually"
 echo '===================================================================================='
 git add -f convertedVideos.json
-git commit -m "Videos processed by the run"
+git commit -m "workflow-run: Videos processed"
 git push
 
 echo 
@@ -26,7 +26,7 @@ for i in $(ls | grep episode); do
     # Stage file for commit
     git add -f episode.json
     # Commit
-    git commit -m 'Uploading Episode to AnchorFM'
+    git commit -m 'workflow-run: Uploading Episode to AnchorFM'
     # Push to trigger
     git push
 done
