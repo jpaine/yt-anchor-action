3e03fdad91eace64fbfd347f9fa8ace67d3a8712
Mandar Devarshi <mandardevarshi@gmail.com>
2022-12-29 19:25:49 +0530

    add: new script to input url

--- triggerRun.sh
+++ triggerRun.sh
@@ -14,9 +14,9 @@ echo
 echo "=======           Commit Changes to Processed Videos                        ======\n"
 echo " Note: To reset in case of failure use emptyProcessedFile script and commit manually"
 echo '===================================================================================='
-# git add -f convertedVideos.json
-# git commit -m "workflow-run: Videos processed"
-# git push
+git add -f convertedVideos.json
+git commit -m "workflow-run: Videos processed"
+git push
 
 echo 
 echo '==================================================='
@@ -25,13 +25,13 @@ echo '==================================================='
 echo 
 echo Total episode to convert and upload: ${TOTAL_EPISODES}
 
-# for i in $(ls | grep episode); do
-#     # Rename to episode.json
-#     mv $i episode.json
-#     # Stage file for commit
-#     git add -f episode.json
-#     # Commit
-#     git commit -m 'workflow-run: Uploading Episode to AnchorFM'
-#     # Push to trigger
-#     git push
-# done
+for i in $(ls | grep episode); do
+    # Rename to episode.json
+    mv $i episode.json
+    # Stage file for commit
+    git add -f episode.json
+    # Commit
+    git commit -m 'workflow-run: Uploading Episode to AnchorFM'
+    # Push to trigger
+    git push
+done
