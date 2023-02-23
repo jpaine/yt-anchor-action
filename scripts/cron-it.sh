60515ab4cd2b0a855fb170866b9d08b429de210b
Mandar Devarshi <mandardevarshi@gmail.com>
2023-02-23 14:43:45 +0530

    test: new workflow

--- scripts/cron-it.sh
+++ scripts/cron-it.sh
@@ -11,4 +11,4 @@ LOG_FILE=$(pwd)/logfile.log
 echo $LOG_FILE
 
 # Schedule the script to run every Thursday at 12 PM UTC
-(crontab -l 2>/dev/null; echo "45 2 * * 4 $SCRIPT_PATH >> $LOG_FILE") | crontab -
+(crontab -l 2>/dev/null; echo "45 14 * * 4 $SCRIPT_PATH >> $LOG_FILE") | crontab -
