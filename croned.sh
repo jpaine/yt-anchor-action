24669522ec98cfd607c79cc8e850fd3ef57997f7
Mandar Devarshi <mandardevarshi@gmail.com>
2023-02-23 15:14:59 +0530

    test:workflow

--- croned.sh
+++ croned.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Get a list of branches and store them in branches.txt
-git branch --list --format='%(refname:lstrip=2)' > branches.txt
+# git branch --list --format='%(refname:lstrip=2)' > branches.txt
 
 # Loop through each branch and switch to it
-while read branch; do
-  git switch "$branch"
-  echo "Switched to branch: $branch"
+# while read branch; do
+#   git switch "$branch"
+#   echo "Switched to branch: $branch"
   
-  # Run git branch --show-current and echo hello
-  echo "Current branch: $(git branch --show-current)"
-  echo "Hello"
+#   # Run git branch --show-current and echo hello
+#   echo "Current branch: $(git branch --show-current)"
+#   echo "Hello"
 
-  # Run the program
-  .$(pwd)/hello.sh
-done < branches.txt
-
+#   # Run the program
+#   .$(pwd)/hello.sh
+# done < branches.txt
+git switch local-cron
+./hello.sh
 # Clean up by removing branches.txt
-rm branches.txt
+# rm branches.txt
--- scripts/cron-it.sh
+++ scripts/cron-it.sh
@@ -11,4 +11,4 @@ LOG_FILE=$(pwd)/logfile.log
 echo $LOG_FILE
 
 # Schedule the script to run every Thursday at 12 PM UTC
-(crontab -l 2>/dev/null; echo "00 15 * * 4 $SCRIPT_PATH > $LOG_FILE 2>&1") | crontab -
+(crontab -l 2>/dev/null; echo "20 15 * * 4 $SCRIPT_PATH > $LOG_FILE 2>&1") | crontab -
