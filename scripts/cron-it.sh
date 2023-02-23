5cf894937a12f5ae190f1ece1280dd1ae655244c
Mandar Devarshi <mandardevarshi@gmail.com>
2023-02-23 14:38:28 +0530

    test: new workflow

--- scripts/cron-it.sh
+++ scripts/cron-it.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to the script to be run
-SCRIPT_PATH=$(pwd)/triggerRun.sh
+SCRIPT_PATH=$(pwd)/croned.sh
 
 echo $SCRIPT_PATH
 
@@ -12,14 +12,3 @@ echo $LOG_FILE
 
 # Schedule the script to run every Thursday at 12 PM UTC
 (crontab -l 2>/dev/null; echo "25 2 * * 4 $SCRIPT_PATH >> $LOG_FILE") | crontab -
-
-# # Check if the script was already run today
-# if grep -q $(date +%F) "$LOG_FILE"; then
-#     echo "Script already run today. Exiting."
-#     exit
-# fi
-
-# Run the script and log its output
-echo "Running script..."
-# $SCRIPT_PATH >> $LOG_FILE 2>&1
-echo "Script run complete."
